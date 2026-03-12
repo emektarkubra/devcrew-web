985c9aa7a6ec8f7aec221dab5a2d1f945c7c6793
Kübra Emektar <124355274+emektarkubra@users.noreply.github.com>
2026-03-12 11:43:51 +0300

    created base-project-code with keycloack

---
+++ public/config.js
@@ -0,0 +1,7 @@
+//paths
+window.API_BASE_URL = 'http://localhost:5173/'
+window.KEYCLOAK_URL = 'http://localhost:8080/';
+window.KEYCLOAK_REALM = 'kubra-realm';
+window.KEYCLOAK_CLIENT = 'kubra-client';
+
+// keys 
