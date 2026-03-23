661c44a9786c9c80bbe960e658ec26006bb4b82c
Kübra Emektar <124355274+emektarkubra@users.noreply.github.com>
2026-03-23 12:27:35 +0300

    updated theme config

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,42 +1,16 @@
-import { Switch } from 'antd'
-import { useEffect, useState } from 'react';
+import { Switch } from 'antd';
+import { useTheme } from '../context/themeContext';
 
 const ThemeSwitch = () => {
-
-    const [theme, setTheme] = useState("light");
-    const [checked, setChecked] = useState(false)
-
-    const handleToggleChange = (checked: boolean) => {
-        setTheme(checked ? 'dark' : 'light');
-        localStorage.setItem('theme', JSON.stringify(checked ? 'dark' : 'light'))
-    };
-
-    useEffect(() => {
-        const themeItem = localStorage.getItem("theme");
-        if (themeItem) {
-            const storedTheme = JSON.parse(themeItem);
-            setTheme(storedTheme)
-        } else {
-            console.log("No theme found in localStorage");
-        }
-    }, []);
-
-    useEffect(() => {
-        const htmlElement = document.querySelector('html');
-        if (htmlElement) {
-            if (theme === 'dark') {
-                htmlElement.classList.add('dark');
-                setChecked(true)
-            } else {
-                htmlElement.classList.remove('dark');
-                setChecked(false)
-            }
-        }
-    }, [theme]);
+    const { isDark, setTheme } = useTheme();
 
     return (
-        <Switch size="small" checked={checked} className='dark:bg-slate-900' onChange={handleToggleChange} />
-    )
-}
+        <Switch
+            size="small"
+            checked={isDark}
+            onChange={(checked) => setTheme(checked ? 'dark' : 'light')}
+        />
+    );
+};
 
-export default ThemeSwitch
+export default ThemeSwitch;
--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState, createContext } from 'react';
 import { api } from '../services';
+import { Spin } from 'antd';
+import toast from 'react-hot-toast';
 
 export type AuthContextProps = {
     user: any | null;
     token: string | null;
     isAuthenticated: boolean;
     logout: () => void;
+    loading: boolean
 };
 
 export const AuthContext = createContext<AuthContextProps>({
@@ -13,6 +16,8 @@ export const AuthContext = createContext<AuthContextProps>({
     token: null,
     isAuthenticated: false,
     logout: () => { },
+    loading: false
+
 });
 
 const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
@@ -20,42 +25,77 @@ const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => 
     const [token, setToken] = useState<string | null>(localStorage.getItem('dt-token'));
     const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        const newToken = api.login.handleCallback();
-        if (newToken) setToken(newToken);
-        else setLoading(false);
-    }, []);
 
-    useEffect(() => {
+
+    const getNewToken = () => {
+        const params = new URLSearchParams(window.location.search)
+        const token = params.get('token')
+
+        if (token) {
+            localStorage.setItem('dt-token', token)
+            window.history.replaceState({}, '', '/overview')
+            setToken(token)
+        }
+    }
+
+
+    const getUserProfile = async () => {
         if (!token) {
             setLoading(false);
             return;
-        }
-        api.login.getUserProfile(token)
-            .then((r: any) => {
-                setUser(r.data)
-                setLoading(false)
-            })
-            .catch(() => {
+        } else {
+            try {
+                const { data, error } = await api.login.getUserProfile(token)
+                if (error) {
+                    toast.error(error)
+                } else {
+                    setUser(data)
+                    setLoading(false)
+                }
+            } catch (error) {
                 localStorage.removeItem('dt-token');
                 setToken(null);
+
+            } finally {
                 setLoading(false);
-            });
+            }
+        }
+
+    }
+
+    useEffect(() => {
+        getNewToken()
+    }, []);
+
+    useEffect(() => {
+        getUserProfile()
     }, [token]);
+
+
 
     const logout = () => {
         api.login.logout();
         setUser(null);
         setToken(null);
-        localStorage.removeItem('dt-token') 
+        localStorage.removeItem('dt-token')
         window.location.href = '/'
     };
 
+
     return (
-        <AuthContext.Provider value={{ user, token, isAuthenticated: !!token && !!user, logout }}>
-            {loading ? null : children}
+        <AuthContext.Provider value={{ user, token, isAuthenticated: !!token && !!user, loading, logout }}>
+            {loading ? (
+                <div style={{
+                    height: '100vh',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                    <Spin size="large" />
+                </div>
+            ) : children}
         </AuthContext.Provider>
-    );
+    )
 };
 
 export default AuthProvider;
