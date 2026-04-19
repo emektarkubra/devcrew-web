18fcc5068e3996657de9eb85d89be209c357820d
Kübra Emektar <124355274+emektarkubra@users.noreply.github.com>
2026-04-19 12:13:59 +0300

    removed keycloack config

--- public/config.js
+++ public/config.js
@@ -1,7 +1,1 @@
-//paths
-window.API_BASE_URL = 'http://localhost:5173/'
-window.KEYCLOAK_URL = 'http://localhost:8080/';
-window.KEYCLOAK_REALM = 'kubra-realm';
-window.KEYCLOAK_CLIENT = 'kubra-client';
-
-// keys 
+window.API_BASE_URL = "http://localhost:8000"
